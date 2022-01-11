70f03ffac4eb55b13a52edfcc6187cf507793627
kmilchev <kmilchev@student.42wolfsburg.de>
2022-01-11 15:30:28 +0100

    works with more than 2 two processes

--- main.c
+++ main.c
@@ -31,38 +31,6 @@ void	print_strings_array(int count, char **args)
 	}
 }
 
-char	*get_input_file(char **arguments)
-{
-	char	*input_file;
-
-	input_file = arguments[1];
-	return (input_file);
-}
-
-char	*get_output_file(int argc, char **arguments)
-{
-	char	*output_file;
-
-	output_file = arguments[argc - 1];
-	return (output_file);
-}
-
-char	**get_commands(int argc, char *argv[])
-{
-	char	**arr;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 2;
-	arr = malloc((argc - 2) * sizeof(char *)); //-2 че да добавя NULL at the end
-	while (j < argc - 1)
-	{
-		arr[i++] = argv[j++];
-	}
-	arr[i] = NULL;
-	return (arr);
-}
 
 void	execute_command(char *argv)
 {
@@ -88,39 +56,49 @@ void	close_desctiptors(int n_fds, ...)
 		i++;
 	}
 }
-void process_1(int fd_input, int *fd, char *argv[2])
+
+void spawn_process(char *argv[])
 {
-	dup2(fd_input, STDIN_FILENO);
-	dup2(fd[1], STDOUT_FILENO);
-	close_desctiptors(3, fd[0], fd[1], fd_input);
-	execute_command(argv[2]);
+	int pid;
+	int fd[2];
+
+	pipe(fd);
+	printf("%s\n", argv[0]);
+	pid = fork();
+	if (pid == 0)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close_desctiptors(2, fd[0], fd[1]);
+		execute_command(argv[0]);
+	}
+
+	dup2(fd[0], STDIN_FILENO);
+	close_desctiptors(2, fd[0], fd[1]);
+	waitpid(pid, NULL, 0);
 }
 int	main(int argc, char *argv[])
 {
 	int	fd[2];
-	int	pid1;
-	int	pid2;
 	int fd_input;
 	int	fd_out;
 
-	if (argc < 5)
-		return (printf("Error! Too few arguments...\n"));
-	pipe(fd);
-	pid1 = fork();
 	fd_input = open(argv[1], O_RDONLY);
-	if (pid1 == 0)
-		process_1(fd_input, fd, argv);
-	pid2 = fork();
-	fd_out = open(argv[4], O_WRONLY);
-	if (pid2 == 0)
+	fd_out = open(argv[argc - 1], O_WRONLY);
+	dup2(fd_input, STDIN_FILENO);
+	dup2(fd_out, STDOUT_FILENO);
+	close_desctiptors(2, fd_out, fd_input);
+	pipe(fd);
+	int counter = 2;
+	while (counter < argc - 2)
 	{
-		dup2(fd[0], STDIN_FILENO);
-		dup2(fd_out, STDOUT_FILENO);
-		close_desctiptors(3, fd[0], fd[1], fd_out);
-		execute_command(argv[argc - 2]);
+		spawn_process(argv + counter);
+		counter++;
 	}
-	close_desctiptors(3, fd[0], fd[1], fd_out);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
+	execute_command(argv[argc - 2]);
 	return (0);
 }
+
+/* 
+	if (argc < 5)
+		return (printf("Error! Too few arguments...\n")); */
+
